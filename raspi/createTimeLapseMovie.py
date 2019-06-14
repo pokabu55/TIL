503e1ccc13dc809c8738c979163bbc8223ced16c
pokabu55 <okazumaru@gmail.com>
2019-06-14 22:39:05 +0900

    Update createTimeLapseMovie.py

--- raspi/createTimeLapseMovie.py
+++ raspi/createTimeLapseMovie.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # 所定のディレクトリの画像から、動画を作るサンプルコード
-
 import cv2
 import glob
 
@@ -8,8 +7,6 @@ import glob
 fileName = glob.glob("./picture/*.jpg")
 # 念の為、ソート
 fileName.sort()
-
-#print(fileName)
 
 # コーデックの種類
 codecs = 'H264'
@@ -20,13 +17,15 @@ imgW = 1280
 imgH = 960
 
 fourcc = cv2.VideoWriter_fourcc(*codecs)
+
 # 保存ファイル名とパラメータの指定
 video = cv2.VideoWriter('timeLapseSky_20fps_full.mp4', fourcc, fps, (imgW, imgH))
 
 for fn in fileName:
-	print(fn)
+	#print(fn)
 	img = cv2.imread(fn)
 	img = cv2.resize(img, (imgW, imgH))
 	video.write(img)
 
 video.release()
+print("done.")
